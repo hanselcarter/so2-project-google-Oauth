bc8aa710a1c22bf4296d4315534416684ab4202e
hanselcarter <barahonahansel@hotmail.com>
2016-10-17 21:07:59 -0300

    Create auth.js

---
+++ auth.js
@@ -0,0 +1,13 @@
+module.exports = {
+	'facebookAuth' : {
+		'clientID': '1811258109155634',
+		'clientSecret': '4d2cc6067ff262e2888fc8bcd6f48b29',
+		'callbackURL': 'http:/localhost:8080/auth/facebook/callback'
+	}, 
+	
+	'googleAuth' : {
+		'clientID': '992537834244-16tqctrm3ogmqlcl2med3hn8no26f292.apps.googleusercontent.com',
+		'clientSecret': 'gTCOnFWhWoDvAAlifbyI9LOl',
+		'callbackURL': 'http://localhost:8080/auth/google/callback'
+	}
+}
